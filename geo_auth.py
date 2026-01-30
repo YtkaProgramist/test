4897a1d497fc2bab25f5b86efe4a3325b2d4c2bd
Utka <91750492+YtkaProgramist@users.noreply.github.com>
2026-01-31 01:02:34 +0600

    Make great def

---
+++ geo_auth.py
@@ -0,0 +1,9 @@
+try:
+    number = int(input("Введите число: "))
+
+    if number >= 50:
+        print("Хихи хаах больше 50!")
+    else:
+        print("О нет! Меньше 50!")
+except ValueError:
+    print("Я же сказал число!")
